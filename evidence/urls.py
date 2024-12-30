2d8bd5e15bfd3d60d40659064dd7f5ee6356b92b
ifrahnz26 <ifrahnz26@gmail.com>
2024-12-30 23:34:00 +0530

    login-signup

--- evidence/urls.py
+++ evidence/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views  # Import views from the evidence app
 
 urlpatterns = [
-    path('', views.landing_page, name='landing_page'),  # Map the root URL to the home view
+    path('', views.home, name='home'),
+      path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),  # Map the root URL to the home view
 ]
--- evidence/views.py
+++ evidence/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
-def landing_page(request):
+def home(request):
     return render(request, 'evidence/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['logemail']
+        password = request.POST['logpass']
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a home or dashboard page after login
+        else:
+            messages.error(request, 'Invalid credentials. Please try again.')
+            return render(request, 'evidence/login.html')
+
+    return render(request, 'evidence/login.html')
+
+# View for the signup page
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You can log in now!')
+            return redirect('login')  # Redirect to login page after successful signup
+        else:
+            messages.error(request, 'There was an error. Please try again.')
+            return render(request, 'evidence/signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+    return render(request, 'evidence/signup.html', {'form': form})
+
+# View for logging out the user
+def logout_view(request):
+    logout(request)
+    return redirect('home')  # Redirect to the login page after logout
